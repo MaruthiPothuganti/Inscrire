85dea34c3a8897e6ca96698e1da096dbdf270182
Maruthi Pothuganti <maruthicharypothuganti@gmail.com>
2022-07-21 10:10:01 +0530

    [style] - SearchBar responsiveness for medium size screens

--- src/Components/SearchInput/SearchInput.jsx
+++ src/Components/SearchInput/SearchInput.jsx
@@ -2,7 +2,7 @@ import { TbAdjustmentsHorizontal } from "../Icons";
 
 export const SearchInput = () => {
   return (
-    <div className="w-3/5">
+    <div className="w-3/4 md:w-3/5">
       <label
         for="default-search"
         className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300"
@@ -35,7 +35,7 @@ export const SearchInput = () => {
         />
         <button
           type="button"
-          className="text-white absolute right-2.5 bottom-2.5  hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="text-white absolute right-2.5 bottom-2.5  hover:bg-blue-800 focus:ring-2 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-400"
         >
           <TbAdjustmentsHorizontal size={20} />
         </button>
